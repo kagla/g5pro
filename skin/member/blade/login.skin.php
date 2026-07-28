8142f466937b0010412ad160099dafdfcdd20331
kagla <kagla@naver.com>
2026-07-29 08:05:49 +0900

    g5blade 로그인: member blade 스킨(login 매핑 + basic 패스스루 스텁) + login 뷰

---
+++ skin/member/blade/login.skin.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+
+g5_view('bbs.login', array(
+    'login_action_url' => $login_action_url,
+    'url'              => $url,
+));
---
+++ skin/member/blade/member_confirm.skin.php
@@ -0,0 +1,4 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+// g5blade: 미변환 화면 — basic 스킨 패스스루
+include(G5_SKIN_PATH.'/member/basic/member_confirm.skin.php');
