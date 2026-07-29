4d8efea0d0028f534b1310e5871e121317b0c14c
kagla <kagla@naver.com>
2026-07-29 14:09:32 +0900

    검색 모달 안내에서 Esc 문구 제거 (동작은 유지)

--- template/one/layout/base.blade.php
+++ template/one/layout/base.blade.php
@@ -126,7 +126,7 @@ var g5_url = "{{ G5_URL }}", g5_bbs_url = "{{ G5_BBS_URL }}", g5_admin_url = "{{
             <input type="text" id="hd_stx" name="stx" maxlength="20" placeholder="무엇을 찾으시나요?" autocomplete="off">
             <button type="submit" class="btn btn-primary">검색</button>
         </form>
-        <p class="search-hint">두 글자 이상 입력하세요<span class="esc-hint"> · <kbd>Esc</kbd> 로 닫기</span></p>
+        <p class="search-hint">두 글자 이상 입력하세요</p>
     </div>
 </div>
 <script>
